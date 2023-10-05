2be249c9531435302081d7e9f1a545b1d4542d58
Marvelous-A <mogneto.r@mail.ru>
2023-10-05 21:50:47 +0300

    Changes to be committed: 	modified:   blog/__pycache__/models.cpython-311.pyc 	new file:   blog/__pycache__/urls.cpython-311.pyc 	new file:   blog/__pycache__/views.cpython-311.pyc 	modified:   blog/models.py 	new file:   blog/static/CSS/blog.css 	new file:   blog/templates/blog/post_list.html 	new file:   blog/urls.py 	modified:   blog/views.py 	modified:   db.sqlite3 	modified:   myblog/__pycache__/settings.cpython-311.pyc 	modified:   myblog/__pycache__/urls.cpython-311.pyc 	modified:   myblog/settings.py 	modified:   myblog/urls.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
+def post_list(request):
+    #posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by("publish_date")
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'blog_posts': posts})
